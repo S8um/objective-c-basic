a8c5794b1c240f7404001bb5a2780b05ca5e26e5
GRoman <grromman@gmail.com>
2021-02-17 12:19:18 +0500

    Init commit lesson 6

---
+++ Lesson_06/Lesson_06/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lesson_06
+//
+//  Created by admin on 17.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}

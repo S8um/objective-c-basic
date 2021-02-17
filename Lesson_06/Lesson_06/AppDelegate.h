02e9d0dc70ce2b288a206a1f6bee46f19bc56f10
GRoman <grromman@gmail.com>
2021-02-17 12:15:31 +0500

    Init commit lesson 6

---
+++ Lesson_06/Lesson_06/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Lesson_06
+//
+//  Created by admin on 17.02.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Lesson_06/Lesson_06/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Lesson_06
+//
+//  Created by admin on 17.02.2021.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
